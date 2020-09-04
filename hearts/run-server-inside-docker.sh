974b8b7ca7ee8811f43398551c97c8ac7ae1cbb1
Mike Sperber <sperber@deinprogramm.de>
2020-09-04 09:35:18 +0200

    Be agostic as to ghc version

--- hearts/run-server-inside-docker.sh
+++ hearts/run-server-inside-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-dist-newstyle/build/x86_64-linux/ghc-8.8.3/hearts-0.1.0.0/x/server/build/server/server -s Table -p 8080 &
-dist-newstyle/build/x86_64-linux/ghc-8.8.3/hearts-0.1.0.0/x/server/build/server/server -s Player -p 8001 -n Mike -i 1 &
-dist-newstyle/build/x86_64-linux/ghc-8.8.3/hearts-0.1.0.0/x/server/build/server/server -s Player -p 8002 -n Peter -i 2 &
-dist-newstyle/build/x86_64-linux/ghc-8.8.3/hearts-0.1.0.0/x/server/build/server/server -s Player -p 8003 -n Nicole -i 3 &
-dist-newstyle/build/x86_64-linux/ghc-8.8.3/hearts-0.1.0.0/x/server/build/server/server -s Player -p 8004 -n Annette -i 4 &
+dist-newstyle/build/x86_64-linux/ghc-*/hearts-0.1.0.0/x/server/build/server/server -s Table -p 8080 &
+dist-newstyle/build/x86_64-linux/ghc-*/hearts-0.1.0.0/x/server/build/server/server -s Player -p 8001 -n Mike -i 1 &
+dist-newstyle/build/x86_64-linux/ghc-*/hearts-0.1.0.0/x/server/build/server/server -s Player -p 8002 -n Peter -i 2 &
+dist-newstyle/build/x86_64-linux/ghc-*/hearts-0.1.0.0/x/server/build/server/server -s Player -p 8003 -n Nicole -i 3 &
+dist-newstyle/build/x86_64-linux/ghc-*/hearts-0.1.0.0/x/server/build/server/server -s Player -p 8004 -n Annette -i 4 &
